5e613d7c07784323a2b13e952c1299ac6dbb84aa
Chad Miller <chad.miller.3333@gmail.com>
2019-01-06 23:51:27 -0500

    Let's try this instead 2

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ function checkLoggedIn(req, res, next)
   if(req.user)
     next()
   else
-    res.status(401).send({ error: 'You are not authorized to upload files.' })
+    res.status(401).end({ error: 'You are not authorized to upload files.' })
 }
 
 module.exports = {
